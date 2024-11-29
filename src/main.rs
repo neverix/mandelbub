55a29ee0aa13cec69c7fdc9f74506459d2a231fd
nev <nev@neverix.io>
2024-11-29 11:34:50 -0500

    Mandelbrot achieved internally

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 use std::fs::File;
 use std::io::BufWriter;
 use std::path::Path;
+use rayon::prelude::*;
 
 fn main() {
     let (w, h) = (512, 512);
-    let pixels = (0..w * h).map(|i| {
+    let pixels = (0..w * h).into_par_iter().map(|i| {
         let (y, x) = (i / h, i % h);
-        if 2 * x + y < 300 {
-            (100, 200, 5)
-        } else {
-            (200, 200, 5)
+        let (y, x) = (y as f32 / h as f32, x as f32 / w as f32);
+        let (y, x) = ((y - 0.5) * 4., (x - 0.5) * 4.);
+
+        let mut a = 0.0;
+        let mut b = 0.0;
+        for _ in 0..100 {
+            // (a+bi)(a+bi)
+            // a^2 - b^2 + 2abi
+            (a, b) = (a*a - b*b + x, 2.*a*b + y);
+            if (a.abs() + b.abs()) > 2. {
+                return (255, 255, 255);
+            }
         }
+
+        (5, 5, 5)
     });
     let out_file = File::create(Path::new("assets/out.png")).unwrap();
     let out_writer = BufWriter::new(out_file);
